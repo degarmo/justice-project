9c017c00bf3abb8bd03fe193cfafdf87f6e96382
CD <cd@CDs-MacBook-Pro.attlocal.net>
2025-05-05 20:27:30 -0500

    memorial page

--- tracker/static/tracker/js/gps_autofill.js
+++ tracker/static/tracker/js/gps_autofill.js
@@ -1,14 +1,22 @@
 
-window.onload = function () {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`)
-                .then(res => res.json())
-                .then(data => {
-                    document.querySelector('#id_city').value = data.city || '';
-                    document.querySelector('#id_state').value = data.principalSubdivision || '';
-                });
-        });
+document.getElementById('share-location').addEventListener('change', function() {
+    if (this.checked) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                document.getElementById('latitude').value = position.coords.latitude;
+                document.getElementById('longitude').value = position.coords.longitude;
+            }, function(error) {
+                alert("Location permission denied or unavailable.");
+                // Optional: uncheck the box or notify user
+                document.getElementById('share-location').checked = false;
+            });
+        } else {
+            alert("Geolocation is not supported by your browser.");
+        }
+    } else {
+        // Reset location fields if unchecked
+        document.getElementById('latitude').value = '';
+        document.getElementById('longitude').value = '';
     }
-};
+});
 
